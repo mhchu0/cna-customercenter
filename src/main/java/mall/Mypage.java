044a7591d0521f15dfc38b4e1d24d91d19bf0c88
추민호(Choo min ho)/정보기술팀/SK EAA427S <AA427@skens.com>
2020-09-25 11:26:22 +0900

    1st commit

---
+++ src/main/java/mall/Mypage.java
@@ -0,0 +1,72 @@
+package mall;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="Mypage_table")
+public class Mypage {
+
+        @Id
+        @GeneratedValue(strategy=GenerationType.AUTO)
+        private Long id;
+        private String productId;
+        private String status;
+        private Integer qty;
+        private Long deliveryId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public Long getDeliveryId() {
+        return deliveryId;
+    }
+
+    public void setDeliveryId(Long deliveryId) {
+        this.deliveryId = deliveryId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    private Long orderId;
+
+
+
+
+
+}
---
+++ src/main/java/mall/MypageViewHandler.java
@@ -0,0 +1,78 @@
+package mall;
+
+import mall.config.kafka.KafkaProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MypageViewHandler {
+
+
+    @Autowired
+    private MypageRepository mypageRepository;
+
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenOrdered_then_CREATE_1 (@Payload Ordered ordered) {
+        try {
+            if (ordered.isMe()) {
+                // view 객체 생성
+                Mypage mypage = new Mypage();
+                // view 객체에 이벤트의 Value 를 set 함
+                mypage.setId(ordered.getId());
+                mypage.setProductId(ordered.getProductId());
+                mypage.setDeliveryId(ordered.getId());
+                mypage.setQty(ordered.getQty());
+                // view 레파지 토리에 save
+                mypageRepository.save(mypage);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenShipped_then_UPDATE_1(@Payload Shipped shipped) {
+        try {
+            if (shipped.isMe()) {
+                // view 객체 조회
+                Optional<Mypage> mypageOptional = mypageRepository.findById(shipped.getId());
+                if( mypageOptional.isPresent()) {
+                    Mypage mypage = mypageOptional.get();
+                    // view 객체에 이벤트의 eventDirectValue 를 set 함
+                    mypage.setDeliveryId(shipped.getId());
+                    mypage.setStatus(shipped.getStatus());
+                    // view 레파지 토리에 save
+                    mypageRepository.save(mypage);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenDeliveryCancelled_then_UPDATE_2(@Payload DeliveryCancelled deliveryCancelled) {
+        try {
+            if (deliveryCancelled.isMe()) {
+                // view 객체 조회
+                List<Mypage> mypageList = mypageRepository.findByOrderId(deliveryCancelled.getOrderId());
+                for(Mypage mypage : mypageList){
+                    // view 객체에 이벤트의 eventDirectValue 를 set 함
+                    mypage.setStatus(deliveryCancelled.getStatus());
+                    // view 레파지 토리에 save
+                    mypageRepository.save(mypage);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
